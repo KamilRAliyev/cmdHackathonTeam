6fe89e1cd0c202e9429f903d27176a0313cf476d
Kamil Aliyev <kamilraliyev@gmail.com>
2020-03-01 05:50:21 +0400

    landing done

--- smecube/urls.py
+++ smecube/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('base.urls')),
-    path('',include('enterprenour.urls')),
     path('admin/', admin.site.urls),
 ]
 
